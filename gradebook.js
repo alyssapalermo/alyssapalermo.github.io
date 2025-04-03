60eecc4c82218ab881eee30ce7826120f674c610
alyssapalermo <43541985+alyssapalermo@users.noreply.github.com>
2025-04-02 21:05:08 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -1,1 +1,11 @@
 
+function fetchGradeData() {
+    console.log("Fetching grade data...");
+}
+
+function populateGradebook() {
+    console.log("Populating gradebook with data...");
+}
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
